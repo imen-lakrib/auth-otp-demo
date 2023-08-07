620a606f1c68bbc718856ec89d861de49dac09b6
imen-lakrib <lak12imen@gmail.com>
2023-08-07 17:53:21 +0100

    fix typo

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,7 +4,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KRY,
+  apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
